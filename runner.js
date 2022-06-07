b675c952923ab3325778b05ce73d562fed65c740
Jordan Wills <jordan.wills@gmail.com>
2022-06-07 12:48:08 +0100

    Fixes

--- runner.js
+++ runner.js
@@ -14,7 +14,14 @@ const getTests = async (token) => {
       "Content-Type": "application/json",
     },
   })
-    .then((response) => response.json())
+    .then((response) => {
+    //   console.log([
+    //     "GET",
+    //     `https://api.drivingtestnow.co.uk/account`,
+    //     response?.status,
+    //   ]);
+      return response.json();
+    })
     .then((data) => {
       return data;
     });
@@ -33,6 +40,11 @@ const bookTests = async (token, testSlotId) => {
       "Content-Type": "application/json",
     },
   });
+//   console.log([
+//     "POST",
+//     `https://api.drivingtestnow.co.uk/booktestslot`,
+//     response?.status,
+//   ]);
   return response;
 };
 
@@ -51,16 +63,21 @@ const countArray = async (arr) => {
 let count = 0;
 const dayInMs = 86400000;
 const recurGetTests = () => {
+  
   getTests(token).then((res) => {
-    console.clear();
+      console.clear();
     const testSlots = res?.earlierTestSlots;
 
+    let attemptedBooking = false;
     count++;
     countArray(testSlots).then((arr) => {
       if (arr.length === 0) {
-        console.log(`Checked available driving tests ${count} time(s)\nChecked through ${testSlots.length} potential test(s)`);
+        console.log(
+          `Checked available driving tests ${count} time(s)\nChecked through ${testSlots.length} potential test(s)`
+        );
         recurGetTests();
       } else {
+        console.log(`Checked available driving tests ${count} time(s)\nChecked through ${testSlots.length} potential test(s)`);
         arr.forEach((entry) => {
           let bookingDate = entry?.datetimeMilliSeconds;
           let minBookingDate = bookingDate + dayInMs * minDays;
@@ -69,11 +86,10 @@ const recurGetTests = () => {
           if (bookingDate > minBookingDate && bookingDate < maxBookingDate) {
             console.log(entry);
             // bookTests(token, entry?._id).then((res) => console.log(res));
-            return;
-          } else {
-            recurGetTests();
-          }
+            attemptedBooking = true;
+          } 
         });
+        attemptedBooking ? null : recurGetTests();
       }
     });
   });
